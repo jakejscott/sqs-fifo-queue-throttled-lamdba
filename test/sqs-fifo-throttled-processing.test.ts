c91b12a755281849624276d5f62856b509f09329
Jake Scott <jake.net@gmail.com>
2021-12-10 11:23:29 +1300

    Initial commit

---
+++ test/sqs-fifo-throttled-processing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsFifoThrottledProcessing from '../lib/sqs-fifo-throttled-processing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs-fifo-throttled-processing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsFifoThrottledProcessing.SqsFifoThrottledProcessingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
